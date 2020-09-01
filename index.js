323df4437597bad057f8d796e9ca52d0329b8551
Renan Carvalho <rcrvlh90@gmail.com>
2020-09-01 16:51:53 -0300

    Compara datas
    Compara data inserida  com atual para validação

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 function comecar() {
     let assunto = document.getElementById("assuntotxt")
     let data = document.getElementById("datatxt")
-    const dataAtual = Date.now()
+    const agora = new Date()
 
     //verifica se algo foi inserido no campo assunto
     if (assunto.value.length == 0) {
         alert("[ERRO]: Por favor, insira um assunto.")
     } else {
         //verifica se a data do assunto em questão foi inserida
-        if (data.value.length == 0 || data.value < Date.now()) {
+        const dataUser = new Date(data.value)
+            //transforma a data e hora inserida em milisegundos para comparar com a data e hora atual
+        if (dataUser.getTime() < agora) {
             alert("[ERRO]: Por favor, insira uma data válida.")
         } else {
             alert("Tudo OK")
         }
     }
-
 }
